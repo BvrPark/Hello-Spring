0cf54a623e2ff3a048366c1373f4019e24cb0967
crupy <wlxh95@naver.com>
2021-09-27 23:32:24 +0900

    HelloController에 hello-mvc 추가
    이전과는 다르게 19번째 줄에 helloMvc뒤에 @RequestParam가 들어가는데 이것도 마찬가지로 ("name")에서 name이 키값이 되고
    그 뒤의 String name의 name은 value값이 된다. 그래서 밑에 줄의 addAttribute에서 앞은 "name"이 키값이 설정되고 뒤의 name은 value가 되는데
    여기서 name은 파라미터여서 따로 입력이 필요한데 이것을 localhost:0000/hello-mvc?name=text와 같은 형식으로 text를 입력해줘야 설정이 된다.

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller     //Spring은 @Controller라고 꼭 적어줘야 함
 public class HelloController {
@@ -12,5 +13,11 @@ public class HelloController {
         model.addAttribute("data", "Hello!!"); //key는 data value는 Hello!!
         return "hello";     //templates/hello.html에게 model과 함께 랜더링해줘라
                             //기본설정이 templates안의 ViewName("text").html을 찾는 것으로 설정
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }

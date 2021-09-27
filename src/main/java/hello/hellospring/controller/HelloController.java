8dd73debf2b27d1e43de255b49214f5cecbbbcfd
crupy <wlxh95@naver.com>
2021-09-27 21:29:02 +0900

    Controller 생성
    hello.html을 controll해주는 controller 생성

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,16 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller     //Spring은 @Controller라고 꼭 적어줘야 함
+public class HelloController {
+
+    @GetMapping("hello")    //웹 어플리케이션에서 /hello라고 들어오면 이 메서드를 호출
+    public String Hello(Model model){
+        model.addAttribute("data", "Hello!!"); //key는 data value는 Hello!!
+        return "hello";     //templates/hello.html에게 model과 함께 랜더링해줘라
+                            //기본설정이 templates안의 ViewName("text").html을 찾는 것으로 설정
+    }
+}
